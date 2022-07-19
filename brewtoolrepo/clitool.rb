c0ef1d68c2036e4aefe5aae861a7c2d70bc58e39
ambhar <amol.freelancing@gmail.com>
2022-07-19 16:15:36 +0530

    Version 0.2.0

--- brewtoolrepo/clitool.rb
+++ brewtoolrepo/clitool.rb
@@ -2,7 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Clitool < Formula
-    desc ""
+    desc "clitool"
     homepage ""
     url "https://github.com/ambhar/clitool/archive/refs/tags/v0.0.2.tar.gz"
     sha256 "ae3931c81931a7c5f1746b9ca6d67c2d3cd0f767419b53d68143dcf2d4b04918"
@@ -11,6 +11,7 @@ class Clitool < Formula
     # depends_on "cmake" => :build
   
     def install
+      bin.install "clitool"
       # ENV.deparallelize  # if your formula fails when building in parallel
       # Remove unrecognized options if warned by configure
       # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
