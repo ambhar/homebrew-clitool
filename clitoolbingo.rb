7a8f512f7141f0f5de386d8c496067fd37817f50
ambhar <amol.freelancing@gmail.com>
2022-07-21 16:01:17 +0530

    whithout sha

--- clitoolbingo.rb
+++ clitoolbingo.rb
@@ -5,10 +5,10 @@ class Clitoolbingo < Formula
     include Language::Python::Virtualenv
     desc "clitoolbingo"
     homepage "https://github.com/ambhar/clitoolbingo"
-    url "https://github.com/ambhar/clitoolbingo/archive/refs/tags/v5.1.tar.gz"
-    sha256 "15a48188efcc9a4cc0cab907523c6e2e935662c1e24c64fe7e1b08cc52cc0f77"
+    url "https://github.com/ambhar/clitoolbingo/archive/refs/tags/v5.1.tar.gz :using => GitHubPrivateRepositoryReleaseDownloadStrategy"
+    # sha256 "15a48188efcc9a4cc0cab907523c6e2e935662c1e24c64fe7e1b08cc52cc0f77"
     license "MIT"
-    depends_on "python@3.9"
+    depends_on "python@3.8"
     resource "click" do
       url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
       sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
