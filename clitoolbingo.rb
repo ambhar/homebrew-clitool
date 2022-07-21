84aeac227a154abe720e32564f272b74bd6629ce
ambhar <amol.freelancing@gmail.com>
2022-07-21 16:04:13 +0530

    syntax

--- clitoolbingo.rb
+++ clitoolbingo.rb
@@ -5,7 +5,7 @@ class Clitoolbingo < Formula
     include Language::Python::Virtualenv
     desc "clitoolbingo"
     homepage "https://github.com/ambhar/clitoolbingo"
-    url "https://github.com/ambhar/clitoolbingo/archive/refs/tags/v5.1.tar.gz , :using => GitHubPrivateRepositoryReleaseDownloadStrategy"
+    url "https://github.com/ambhar/clitoolbingo/archive/refs/tags/v5.1.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     # sha256 "15a48188efcc9a4cc0cab907523c6e2e935662c1e24c64fe7e1b08cc52cc0f77"
     license "MIT"
     depends_on "python@3.8"
