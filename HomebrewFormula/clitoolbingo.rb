c787aa2ed5c55c7b0234debba074c3125392d815
ambhar <amol.freelancing@gmail.com>
2022-07-21 17:41:17 +0530

    tool

--- HomebrewFormula/clitoolbingo.rb
+++ HomebrewFormula/clitoolbingo.rb
@@ -9,7 +9,7 @@ class Clitoolbingo < Formula
     homepage "https://github.com/ambhar/clitoolbingo"
     url "https://github.com/ambhar/clitoolbingo", :using => :git,:tag=>'v5.2'
     # https://github.com/<owner>/<repo>/releases/download/<version>/<file>"
-    # sha256 "15a48188efcc9a4cc0cab907523c6e2e935662c1e24c64fe7e1b08cc52cc0f77"
+    sha256 "15a48188efcc9a4cc0cab907523c6e2e935662c1e24c64fe7e1b08cc52cc0f77"
     license "MIT"
     depends_on "python@3.8"
     resource "click" do
