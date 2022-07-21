e90e55e9f5e3f44c5e8f64725bd19c0baed54507
ambhar <amol.freelancing@gmail.com>
2022-07-21 18:24:02 +0530

    python version

--- HomebrewFormula/clitoolbingo.rb
+++ HomebrewFormula/clitoolbingo.rb
@@ -9,16 +9,16 @@ class Clitoolbingo < Formula
     homepage "https://github.com/ambhar/clitoolbingo"
     url "https://github.com/ambhar/clitoolbingo", :using => :git, :tag => "v5.1"
     # https://github.com/<owner>/<repo>/releases/download/<version>/<file>"
-    sha256 "15a48188efcc9a4cc0cab907523c6e2e935662c1e24c64fe7e1b08cc52cc0f77"
+    # sha256 "15a48188efcc9a4cc0cab907523c6e2e935662c1e24c64fe7e1b08cc52cc0f77"
     license "MIT"
     depends_on "python@3.8"
     resource "click" do
       url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
-      # sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+      sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
     end
 
     def install
-      system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+      system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
 
       virtualenv_install_with_resources
     end
